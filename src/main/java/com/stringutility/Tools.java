3c5bf5bb1534715fa700c0f5298e58b7e556e4f5
TeddyLin <teddy.l@mig.me>
2017-03-07 18:36:36 +0800

    String Utility 0.0.1

---
+++ src/main/java/com/stringutility/Tools.java
@@ -0,0 +1,12 @@
+package com.stringutility;
+
+/**
+ * Created by teddylin on 07/03/2017.
+ */
+
+public class Tools {
+
+    public static String trimAll(String text) {
+        return text.trim();
+    }
+}
